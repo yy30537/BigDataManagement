ca76179b35fd03a0b5db0cbdd28032fce0c7fe53
yy30537 <yang30537@gmail.com>
2023-03-02 13:58:35 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,9 +11,12 @@ import scala.Tuple2;
 import org.apache.spark.api.java.JavaPairRDD;
 import java.util.Arrays;
 import org.apache.spark.sql.types.*;
+import org.apache.spark.sql.functions.*;
 import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.split;
 import static org.apache.spark.sql.functions.sum;
+import static org.apache.spark.sql.functions.countDistinct;
+import org.apache.spark.sql.api.java.*;
 
 
 
@@ -48,20 +51,17 @@ public class Main {
             .withColumn("_c1_array", split(col("_c1"), ";").cast(DataTypes.createArrayType(DataTypes.IntegerType)))
             .drop("_c1");
 
-        System.out.println("Excerpt of the dataframe content:");
-        //df.show(10);
-        
+        System.out.println("dataframe content:");
+        df.show(10);
         System.out.println("Dataframe's schema:");
-        //df.printSchema();
+        df.printSchema();
         
         return df;
-
     }
 
     private static JavaPairRDD q1b(JavaSparkContext sparkContext, boolean onServer) {
         String vectorsFilePath = (onServer) ? "/vectors.csv" : "vectors.csv";
 
-    
         JavaPairRDD<String, int[]> vectors = sparkContext.textFile(vectorsFilePath)
         .mapToPair(line -> {
             String[] row = line.split(",");
@@ -72,92 +72,44 @@ public class Main {
             return new Tuple2<>(key, vector);
         });
 
-
-        // List<Tuple2<String, int[]>> list = vectors.take(10);
-        // for (Tuple2<String, int[]> pair : list) {
-        //     System.out.println("Key: " + pair._1());
-        //     System.out.println("Value: " + Arrays.toString(pair._2()));
-        // }
-
         return vectors;
-    
     }
 
 
-
-    /* 
-        Sudo code
-
-        ===== iterativly select triples of vectors from Dataset
-            X Y Z                
-
-        DXNT 42;45;51;41;17
-        [key1] x1;x2  ...  xi
-        [key2] y1;y2  ...  yi
-        [key3] z1;z2  ...  zi
-
-
-        (For i = 1 to 250)
-            1. Calculate Aggregate
-            A = {x1+y1+z1, x2+y2+z2 ... xi+yi+zi}
-
-            2. Calculate Variance 
-            int l = A.length;
-            i = (1...l)
-            sum = A[1]^2 + A[2]^2 +... + A[i]^2
-                = (x1+y1+z1)^2 + (x2+y2+z2)^2 +... + (xi+yi+zi)^2
-
-            j = (1...l)
-            mu = (A[1] + A[2] +... + A[j]) / l
-            mu = [(x1+y1+z1) + (x2+y2+z2) + ... + (xi+yi+zi)] / l
-
-            V = (1/l)*sum - mu^2
-            τ = {20,50,310,360,410}
-            V ? τ
-
-        */
+   
     private static void q2(JavaSparkContext sparkContext, Dataset dataset) { 
         //int tau = 20;
         SparkSession sparkSession = SparkSession.builder().appName("example").getOrCreate();
         dataset.createOrReplaceTempView("vectors");
 
-        // join the vectors table with itself three times by distinct keys, get rid of duplicates
-        // thus finding the distinct triplets
-        Dataset<Row> x = sparkSession.sql("SELECT _c0 AS x_key, _c1_array AS x_array FROM vectors");
-        Dataset<Row> y = sparkSession.sql("SELECT _c0 AS y_key, _c1_array AS y_array FROM vectors");
-        Dataset<Row> z = sparkSession.sql("SELECT _c0 AS z_key, _c1_array AS z_array FROM vectors");
-        
-        Dataset<Row> distinct_triples = x.crossJoin(y).crossJoin(z)
-        .where("x_key != y_key AND x_key != z_key AND y_key != z_key") 
-        .selectExpr("x_array", "y_array", "z_array")
-        .distinct();
-        
-        distinct_triples.createOrReplaceTempView("triplets");
+        //System.out.println(dataset.count());
 
-        long count = distinct_triples.count();
+        String query_getTriples = "SELECT a._c0 AS xid, b._c0 AS yid, c._c0 AS zid, a._c1_array AS x, b._c1_array AS y, c._c1_array AS z " +
+        "FROM vectors a, vectors b, vectors c " +
+        "WHERE a._c0 < b._c0 AND b._c0 < c._c0";
 
-        for (int i = 0; i <= 0; i++) {
 
-            String SQL_Aggregate = String.format(
-                "SELECT (x_array[%d] + y_array[%d] + z_array[%d]) AS A FROM triplets", i, i, i);
-            
-            String SQL_Square = String.format(
-                "SELECT pow((x_array[%d] + y_array[%d] + z_array[%d]), 2) AS A_2 FROM triplets", i, i, i);
+        Dataset<Row> triples = sparkSession.sql(query_getTriples);
+        triples.createOrReplaceTempView("triples");
 
-            Dataset<Row> A = sparkSession.sql(SQL_Aggregate);
-            Dataset<Row> A_2 = sparkSession.sql(SQL_Square);
-
-            double sum = ((Number)A_2.agg(sum(col("A_2"))).head().get(0)).doubleValue();
-            double mu = ((Number)A.agg(sum(col("A"))).head().getDouble(0)).doubleValue();
-            //double l = A.count();
-            //mu = mu / l;
-            //double V = (1 / l) * sum - mu * mu;
-
-            System.out.println(sum);
-            System.out.println(mu);
-        }
-    }
+        System.out.println("Number of distinct triples: " + triples.count());
+        triples.show();
     
+        String query_aggregate = ("SELECT " +
+                    "SUM(element_at(x, i) + element_at(y, i) + element_at(z, i)) AS sum " +
+                "FROM (" +
+                    "SELECT x, y, z FROM triples " +
+                ") AS t " +
+                "LATERAL VIEW posexplode(t.x) AS x_exploded(i, x_elem) " +
+                "LATERAL VIEW posexplode(t.y) AS y_exploded(i, y_elem) " +
+                "LATERAL VIEW posexplode(t.z) AS z_exploded(i, z_elem) " +
+                "GROUP BY i");
+
+
+        Dataset<Row> results = sparkSession.sql(query_aggregate);
+        results.show();
+    }
+
     private static void q3(JavaSparkContext sparkContext, JavaRDD rdd) {
         // TODO: Implement Q3 here
     }
@@ -181,8 +133,6 @@ public class Main {
 
         //JavaPairRDD rdd = q1b(sparkContext, onServer);
 
-        
-
         q2(sparkContext, dataset);
 
         // q3(sparkContext, rdd);
@@ -190,9 +140,6 @@ public class Main {
         // q4(sparkContext, rdd);
 
         sparkContext.close();
-
-
-
     }
 }
 
